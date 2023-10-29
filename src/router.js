4ebb56eef1794332481ca0050a634cc047424ec2
LuizHLG <luizlelek08@gmail.com>
2023-10-29 11:28:06 -0300

    banco de dados criado

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const tasksController = require("./controllers/tasksController");
 
-// router.get("/tasks", (req, res) => res.status(200).send("Olá, mundo!"));
+router.get("/tasks", tasksController.getAll);
 
 module.exports = router;
